18954798edaf838660c4d66991dfd4c40a459ea7
prasadssp <78095881+prasadssp@users.noreply.github.com>
2021-04-27 11:35:06 -0500

    Update main.tf

--- CHAP07/terraform-aci/main.tf
+++ CHAP07/terraform-aci/main.tf
@@ -28,7 +28,7 @@ resource "azurerm_container_group" "aci-myapp" {
   os_type             = "linux"
   container {
     name   = "myappdemo"
-    image  = "docker.io/${var.dockerhub-username}/demobook:${var.imageversion}"
+    image  = "docker.io/${var.dockerhub-Username}/demobook:${var.imageversion}"
     cpu    = "0.5"
     memory = "1.5"
 
